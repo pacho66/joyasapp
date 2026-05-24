6a39bd40aad24e95f4fa3cbfb68d8fde203fbae4
Francisco Gonzalez <plateria.gonzalez73@gmail.com>
2026-05-24 11:29:59 -0500

    add gitignore

--- pgjoyasgonzalez/settings.py
+++ pgjoyasgonzalez/settings.py
@@ -28,7 +28,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 SECRET_KEY = 'django-insecure-y!1z==8481!^59e%m^x1&hica@__820%1!w#b4fn@58v$w49(z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'joyasapp.onrender.com',
@@ -131,6 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
