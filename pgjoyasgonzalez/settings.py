6b60a270c9569a4ae0b5f32751a44638b586e92c
Francisco Gonzalez <plateria.gonzalez73@gmail.com>
2026-06-01 18:55:44 -0500

    Integracion Cloudinary

--- pgjoyasgonzalez/settings.py
+++ pgjoyasgonzalez/settings.py
@@ -54,9 +54,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary',
+    'cloudinary_storage',
     'tienda',
     'productos.apps.ProductosConfig',  
 ]
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+}
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
