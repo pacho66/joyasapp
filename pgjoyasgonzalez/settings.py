6bf21fd6353a3958e399ecdd7b79dc2b561e0ac5
Francisco Gonzalez <plateria.gonzalez73@gmail.com>
2026-07-06 06:38:03 -0500

    fix(settings): limpiar sintaxis de instalado de apps y storages

--- pgjoyasgonzalez/settings.py
+++ pgjoyasgonzalez/settings.py
@@ -61,12 +61,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    
-    # 🌟 Solo una vez y antes de staticfiles:
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
-    
     'tienda',
     'productos.apps.ProductosConfig',
     'django.contrib.humanize',   
