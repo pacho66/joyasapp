24e78fd9a90ac92e3936b5aa738dee03e10c1e3d
Francisco Gonzalez <plateria.gonzalez73@gmail.com>
2026-07-06 06:56:33 -0500

    fix(static): cambiar backend de whitenoise para evitar caídas en collectstatic

--- pgjoyasgonzalez/settings.py
+++ pgjoyasgonzalez/settings.py
@@ -80,7 +80,7 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage", 
     },
 }
 #DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
