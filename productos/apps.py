dab705e9f8cecda2573f4ab27588ccea1ea864b7
Francisco Gonzalez <plateria.gonzalez73@gmail.com>
2026-07-06 21:11:02 -0500

    Fix: limpiar apps.py y corregir importación local de signals

--- productos/apps.py
+++ productos/apps.py
@@ -1,12 +1,9 @@
 from django.apps import AppConfig
 
-
-class ProductosConfig(AppConfig):
-    name = 'productos'
-
 class ProductosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'productos'
 
     def ready(self):
-        import productos.signals
+        # 🎯 Cambiamos el import para que use una ruta limpia y el editor lo reconozca
+        from . import signals
